15ddb18a14c51ab7118f737e993e79d6e4f08f19
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-12-18 11:44:57 +0000

    fix: prime check

--- index.js
+++ index.js
@@ -44,11 +44,11 @@ function verifyInput(){
 function checkPrimeNO() {
   if(date[0] % 2 == 0)
     return chalk.red(name + " your birthday isn't a prime number");
-  for(let i = 0; i < Math.sqrt(date[0]); i++){
+  for(let i = 0; i <= Math.sqrt(date[0]); i++){
     if(date[0] % i == 0)
       return chalk.red(name + " your birthday isn't a prime number");
-    return chalk.green(name + " your birthday is a prime number!!");      
   }
+  return chalk.green(name + " your birthday is a prime number!!");      
 }
 
 console.log("\n" + checkPrimeNO());
